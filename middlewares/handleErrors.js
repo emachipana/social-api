e09e58ab1d5fd21f049d5495809ef7453754b1ac
emachipana <emachipana0102@gmail.com>
2022-11-24 18:02:26 -0500

    ADD: handler file not supported

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,8 +1,14 @@
 // possible execution errors
 const ERRORS_STORE = {
+  // handle malformed ObjectId
   CastError: (res) => {
     res.status(400).json({ message: "id used is malformed" });
   },
+  // handle file not supported
+  Error: (res, err) => {
+    res.status(422).json({ message: err.message });
+  },
+  // in case no error match
   defaultError: (res, err) => {
     res.status(500).json({ message: "Unknow error, please try again", detail: err });
   }
@@ -11,6 +17,7 @@ const ERRORS_STORE = {
 function handleErrors(err, _req, res, _next) {
   console.error(err);
 
+  // condition based on error name
   const handler = ERRORS_STORE[err.name] || ERRORS_STORE.defaultError;
   handler(res, err);
 }
