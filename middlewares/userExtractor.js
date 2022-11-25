74ef30de5279dc69d015de7f9382ecd5336adaa7
emachipana <emachipana0102@gmail.com>
2022-11-24 21:51:12 -0500

    ADD: user extractor middleware

---
+++ middlewares/userExtractor.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+function userExtractor(req, res, next) {
+  // get header authorization
+  const authorization = req.get("authorization");
+
+  // get token and handle authorization undefined
+  const token = authorization?.spli(" ")[1];
+
+  // decode token with  json web token
+  const decodedToken = token 
+    ? jwt.verify(token, process.env.TOKEN_SECRET)
+    : {};
+
+  // handle token no provided
+  if(!decodedToken.id) return res.status(401).json({
+    message: "Acces denied, token missing"
+  });
+
+  // get user id from decoded token
+  const { id: userId } = decodedToken;
+
+  // add userId key to request object
+  req.userId = userId;
+
+  next();
+}
+
+export default userExtractor;
