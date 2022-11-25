69c1ebc7724f9fc5387bbfe85a5acefb57fca20b
emachipana <emachipana0102@gmail.com>
2022-11-25 13:42:17 -0500

    FIX: create user on signup endpoint

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import upload from "../middlewares/multer.js";
+import { cloudinary } from "../services/cloudinary.js";
 
 const sessionsRouter = Router();
 
@@ -40,6 +42,65 @@ sessionsRouter.post("/login", async (req, res, next) => {
      // response to client
      res.json({ ...restOfUser, token });
   }catch(err) {
+    next(err);
+  }
+});
+
+// POST create user
+sessionsRouter.post("/signup", upload.single("avatar"), async (req, res, next) => {
+  const { username, name, last_name, password } = req.body;
+  let uploadedImage = "";
+
+  try {
+    // config password hash
+    const saltRounds = 10;
+    const passwordHash = password
+      ? await bcrypt.hash(password, saltRounds)
+      : undefined;
+
+    // upload image to cloudinary
+    if(req.file) {
+      uploadedImage = await cloudinary.uploader.upload(req.file.path);
+    }
+
+    const avatar = uploadedImage 
+      ? {
+          public_id: uploadedImage.public_id,
+          url: uploadedImage.secure_url
+        }
+      : undefined;
+
+    // create user
+    const newUser = new User({
+      username,
+      name,
+      last_name,
+      passwordHash,
+      avatar,
+      date: new Date()
+    });
+
+    // save user
+    const savedUser = await newUser.save();
+
+    // generate token
+    const { id, ...user } = savedUser.toJSON();
+    const infoForToken = { id, username };
+    const token = jwt.sign(
+      infoForToken,
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: 60 * 60 * 24 * 7 // expire at 7 days
+      }
+    );
+
+    // response to client
+    res.status(201).json({ id, ...user, token });
+  }catch(err) {
+    // remove image from cloudinary when app is crashed
+    if(uploadedImage) {
+      await cloudinary.uploader.destroy(uploadedImage.public_id);
+    }
     next(err);
   }
 });
