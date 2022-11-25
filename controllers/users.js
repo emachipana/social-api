9855d277bb48d7df40b6d78722cde1a9dc9669bd
emachipana <emachipana0102@gmail.com>
2022-11-24 21:32:40 -0500

    ADD: create user endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import User from "../models/User.js";
-import upload from "../utils/multer.js";
+import { cloudinary } from "../services/cloudinary.js";
+import upload from "../middlewares/multer.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const usersRouter = Router();
 
@@ -19,6 +22,57 @@ usersRouter.get("/:id", async (req, res, next) => {
   
     res.json(user);
   }catch(err) {
+    next(err);
+  }
+});
+
+// POST create user
+usersRouter.post("/", upload.single("avatar"), async (req, res, next) => {
+  const { username, name, last_name, password } = req.body;
+  let uploadedImage = "";
+
+  try {
+    // config password hash
+    const saltRounds = 10;
+    const passwordHash = password
+      ? await bcrypt.hash(password, saltRounds)
+      : undefined;
+
+    // upload image to cloudinary
+    uploadedImage = await cloudinary.uploader.upload(req.file.path);
+
+    // create user
+    const newUser = new User({
+      username,
+      name,
+      last_name,
+      passwordHash: passwordHash,
+      avatar: {
+        public_id: uploadedImage.public_id,
+        url: uploadedImage.secure_url
+      },
+      date: new Date()
+    });
+
+    // save user
+    const savedUser = await newUser.save();
+
+    // generate token
+    const { id, ...user } = savedUser.toJSON();
+    const infoForToken = { id, username };
+    const token = jwt.sign(
+      infoForToken,
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: 60 * 60 * 24 * 7 // expire at 7 days
+      }
+    )
+
+    // response to client
+    res.status(201).json({ id, ...user, token });
+  }catch(err) {
+    // remove image from cloudinary when app is crashed
+    await cloudinary.uploader.destroy(uploadedImage.public_id);
     next(err);
   }
 });
