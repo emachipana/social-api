f7c3e14798b42527c9629663090fbd116b09ae90
emachipana <emachipana0102@gmail.com>
2022-11-24 22:11:51 -0500

    ADD: delete user endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,8 @@ import { cloudinary } from "../services/cloudinary.js";
 import upload from "../middlewares/multer.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import userExtractor from "../middlewares/userExtractor.js";
+import Post from "../models/Post.js";
 
 const usersRouter = Router();
 
@@ -11,6 +13,8 @@ const usersRouter = Router();
 usersRouter.get("/:id", async (req, res, next) => {
   try {
     // get user from database and populate with her posts
+    const posts = await Post.find({ });
+    console.log(posts);
     const user = await User.findById(req.params.id).populate("posts", {
       content: 1,
       photo: 1,
@@ -66,13 +70,34 @@ usersRouter.post("/", upload.single("avatar"), async (req, res, next) => {
       {
         expiresIn: 60 * 60 * 24 * 7 // expire at 7 days
       }
-    )
+    );
 
     // response to client
     res.status(201).json({ id, ...user, token });
   }catch(err) {
     // remove image from cloudinary when app is crashed
     await cloudinary.uploader.destroy(uploadedImage.public_id);
+    next(err);
+  }
+});
+
+// DELETE user
+usersRouter.delete("/", userExtractor, async (req, res, next) => {
+  const { userId } = req;
+  
+  try {
+    // delete user and get user deleted
+    const user = await User.findByIdAndDelete(userId);
+
+    // handle userId does not match any doc
+    if(!user) return res.status(404).json({ message: "User not found, try again" });
+
+    // remove image from cloudinary
+    await cloudinary.uploader.destroy(user.avatar.public_id);
+
+    // response to client
+    res.json({ message: "User was deleted" });
+  }catch(err) {
     next(err);
   }
 });
