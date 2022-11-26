e8dbeffb29028da0c02601a5e9dd9416753a38b2
emachipana <emachipana0102@gmail.com>
2022-11-25 22:37:23 -0500

    ADD: get one post endpoint

--- controllers/posts.js
+++ controllers/posts.js
@@ -20,4 +20,27 @@ postsRouter.get("/", async (_req, res) => {
   res.json(posts);
 });
 
+// GET post by id
+postsRouter.get("/:id", async (req, res, next) => {
+  try {
+    // get post from database and populate user and likes
+    const post = await Post.findById(req.params.id)
+      .populate("user", {
+        avatar: 1,
+        name: 1,
+        date: 1
+      })
+      .populate("likes", {
+        user: 1
+      });
+
+    // return response not found if post is not found
+    if(!post) return res.status(404).json({ message: "Post not found" });
+
+    res.json(post);
+  }catch(err) {
+    next(err);
+  }
+});
+
 export default postsRouter;
