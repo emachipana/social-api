e54573b17081869506a190848d7d11a49e5dcefa
emachipana <emachipana0102@gmail.com>
2022-11-27 19:34:22 -0500

    FIX: adapt middleware to validate all models

--- middlewares/validateOwnerUser.js
+++ middlewares/validateOwnerUser.js
@@ -1,26 +1,46 @@
+import Comment from "../models/Comment.js";
+import Like from "../models/Like.js";
 import Post from "../models/Post.js";
+
+const MODELS = {
+  posts: Post,
+  comments: Comment,
+  likes: Like
+};
 
 async function validateOwnerUser(req, res, next) {
   const { userId } = req;
-  const { id: postId } = req.params;
+  const { id: docId } = req.params;
 
-  // get post from database
-  const responsePost = await Post.findById(postId);
+  try {
+    // get path from request object
+    const path = req.baseUrl.split("/")[2]; // ej: posts
   
-  // handle postId does not match any doc
-  if(!responsePost) return res.status(404).json({ message: "Post not found" });
+    const Model = MODELS[path];
+  
+    // get doc from database
+    const docResponse = await Model.findById(docId);
+    
+    // delete las letter of path
+    const nameDoc = path.slice(0, -1);
+  
+    // handle docId does not match any doc
+    if(!docResponse) return res.status(404).json({ message: `${nameDoc} not found` });
+  
+    const doc = docResponse.toJSON();
+  
+    // validate user is owner of doc
+    if(!doc.user.equals(userId)) return res.status(401).json({
+      message: `This ${nameDoc} belongs to another user`
+    });
+  
+    // add data to request object
+    req[nameDoc] = doc;
 
-  const post = responsePost.toJSON();
-
-  // validate user is owner of post
-  if(!post.user.equals(userId)) return res.status(401).json({
-    message: "This post belongs to another user"
-  });
-
-  // add data to request object
-  req.post = post;
-
-  next();
+    next();
+  }catch(err) {
+    next(err);
+  }
 }
 
 export default validateOwnerUser;
