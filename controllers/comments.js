638030058618ccdc6d5120a8b4f36822bd87328c
emachipana <emachipana0102@gmail.com>
2022-11-27 19:34:54 -0500

    ADD: update comment endpoint

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authorizeUser from "../middlewares/authorizeUser.js";
+import validateOwnerUser from "../middlewares/validateOwnerUser.js";
 import Comment from "../models/Comment.js";
 import Post from "../models/Post.js";
 import User from "../models/User.js";
 
 const commentsRouter = Router();
 
+// POST create comment
 commentsRouter.post("/", authorizeUser ,async (req, res, next) => {
   const { content, postId } = req.body;
   const { userId } = req;
@@ -36,6 +38,27 @@ commentsRouter.post("/", authorizeUser ,async (req, res, next) => {
 
     // response to client
     res.status(201).json(savedComment);
+  }catch(err) {
+    next(err);
+  }
+});
+
+// PATCH update comment
+commentsRouter.patch("/:id", [ authorizeUser, validateOwnerUser ], async (req, res, next) => {
+  const { content } = req.body;
+
+  try {
+    const commentInfo = { content };
+
+    // update comment
+    const updatedComment = await Comment.findByIdAndUpdate(
+      req.params.id,
+      commentInfo,
+      { new: true }
+    );
+
+    // response to client
+    res.json(updatedComment);
   }catch(err) {
     next(err);
   }
